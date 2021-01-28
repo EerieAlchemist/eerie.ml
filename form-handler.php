5cbc9c24fa9f75813e888069716a781473ef9780
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-01-28 18:30:54 -0500

    class issue fixed

--- form-handler.php
+++
@@ -1,7 +0,0 @@
-<?php // sends contact info from basic form to an email address, WIP
-
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $email_from = 'notpostingthispubliclylol';
