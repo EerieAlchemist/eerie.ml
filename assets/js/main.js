710a4bd0e3445f0d3e481bfc1d9f1b20a38f6cd5
Eerie <66958528+EerieDev@users.noreply.github.com>
2021-03-16 18:39:17 -0400

    updated scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,23 +29,15 @@ const sr = ScrollReveal({
 });
 
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-
+sr.reveal('.button',{delay: 100}); 
+sr.reveal('.home__img',{delay: 100}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
-
+sr.reveal('.about__subtitle',{delay: 100}); 
+sr.reveal('.about__text',{delay: 100}); 
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
-
-sr.reveal('.work__img',{interval: 200}); 
-
-sr.reveal('.contact__input',{interval: 200}); 
-
-
-
-
+sr.reveal('.skills__data',{interval: 100}); 
+sr.reveal('.skills__img',{delay: 100});
+sr.reveal('.work__img',{interval: 100}); 
+sr.reveal('.contact__input',{interval: 100}); 
