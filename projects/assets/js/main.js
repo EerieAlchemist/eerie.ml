99a3cdab01a68dc6a5ded291b2a65ab8570d15c1
Eerie <66958528+EerieDev@users.noreply.github.com>
2021-03-19 09:22:00 -0400

    shortened scroll reveal speed

--- projects/assets/js/main.js
+++ projects/assets/js/main.js
@@ -1,5 +1,3 @@
-// this took me 4 fucking hours, im actually proud of it, thank goodness for docs
-
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -26,28 +24,20 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
+    duration: 25,
     reset: true
 });
 
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-
+sr.reveal('.button',{delay: 25}); 
+sr.reveal('.home__img',{delay: 25}); 
+sr.reveal('.home__social-icon',{interval: 25}); 
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
-
+sr.reveal('.about__subtitle',{delay: 25}); 
+sr.reveal('.about__text',{delay: 25}); 
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
-
-sr.reveal('.work__img',{interval: 200}); 
-
-sr.reveal('.contact__input',{interval: 200}); 
-
-
-
-
+sr.reveal('.skills__data',{interval: 25}); 
+sr.reveal('.skills__img',{delay: 25});
+sr.reveal('.work__img',{interval: 25}); 
+sr.reveal('.contact__input',{interval: 25});
