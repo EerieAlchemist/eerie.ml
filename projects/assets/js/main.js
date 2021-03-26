3136b0621c5062b807122b33ef52fe403990594c
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-03-25 23:25:37 -0400

    updated animations, hrefs, cleanup

--- projects/assets/js/main.js
+++ projects/assets/js/main.js
@@ -1,8 +1,7 @@
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
-
-    if(toggle && nav){
+    if (toggle && nav){
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
         })
@@ -15,7 +14,6 @@ const navLink = document.querySelectorAll('.nav__link');
 function linkAction(){
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
-
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
@@ -24,20 +22,20 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 85,
-    reset: true
+    duration: 250,
+    reset: false
 });
 
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 150}); 
-sr.reveal('.home__img',{delay: 150}); 
-sr.reveal('.home__social-icon',{interval: 150}); 
+sr.reveal('.button',{delay: 250});
+sr.reveal('.home__img',{delay: 250});
+sr.reveal('.home__social-icon',{interval: 250});
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 150}); 
-sr.reveal('.about__text',{delay: 150}); 
+sr.reveal('.about__subtitle',{delay: 250});
+sr.reveal('.about__text',{delay: 250});
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 150}); 
-sr.reveal('.skills__img',{delay: 150});
-sr.reveal('.work__img',{interval: 150}); 
-sr.reveal('.contact__input',{interval: 150});
+sr.reveal('.skills__data',{interval: 250});
+sr.reveal('.skills__img',{delay: 250});
+sr.reveal('.work__img',{interval: 250});
+sr.reveal('.contact__input',{interval: 250});
